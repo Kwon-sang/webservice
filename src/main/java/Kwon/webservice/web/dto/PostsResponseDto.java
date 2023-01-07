6185a5b836aba2b25c78a12bcb45a06c4c2af724
Kwon-sang <kwonsg514@gmail.com>
2023-01-08 00:21:33 +0900

    글쓰기, 수정, 삭제 기능 구현

---
+++ src/main/java/Kwon/webservice/web/dto/PostsResponseDto.java
@@ -0,0 +1,20 @@
+package Kwon.webservice.web.dto;
+
+import Kwon.webservice.domain.posts.Posts;
+import lombok.Getter;
+
+@Getter
+public class PostsResponseDto {
+
+    private Long id;
+    private String title;
+    private String content;
+    private String author;
+
+    public PostsResponseDto(Posts entity) {
+        this.id = entity.getId();
+        this.title = entity.getTitle();
+        this.content = entity.getContent();
+        this.author = entity.getAuthor();
+    }
+}
---
+++ src/test/java/Kwon/webservice/web/HelloControllerTest.java
@@ -0,0 +1,22 @@
+package Kwon.webservice.web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+@WebMvcTest(HelloController.class)
+class HelloControllerTest {
+
+    @Autowired
+    private MockMvc mvc;
+
+    @Test
+    void hello() throws Exception{
+        mvc.perform(MockMvcRequestBuilders.get("/hello"))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.content().string("hello"));
+    }
+}
